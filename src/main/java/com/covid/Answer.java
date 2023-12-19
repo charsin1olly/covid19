2d3228d5a22c2c58f64d1f92054ebdedabdeba08
Charsin Chan <116936412+charsin1olly@users.noreply.github.com>
2023-12-20 00:40:47 +0800

    finish

--- src/main/java/com/covid/Answer.java
+++ src/main/java/com/covid/Answer.java
@@ -1,46 +1,31 @@
 package com.covid;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 
-/*
-1. 加總每個縣市的新冠病毒案例
-2. 根據月份跟縣市加總
-3. 取台北在六月份的新冠病毒案例
- * 
- */
 public class Answer {
     public static void main( String[] args ){
         List<Covid19Case> covid19case = Covid19Statistics.covid19Cases();
-
+        
     	System.out.println("1. 加總每個縣市的新冠病毒案例" + answerOne(covid19case));
-    	
     	System.out.println("2. 根據月份跟縣市加總" + answerTwo(covid19case));
-
     	System.out.println("3. 取台北在六月份的新冠病毒案例" + answerThree(covid19case));
-
-    	
-    	//2. 根據月份跟縣市加總
     }
     
     private static Map<String, Integer>  answerOne(List<Covid19Case> covid19case) {
         //1. 加總每個縣市的新冠病毒案例
-    	Map<String, Integer> caseCount = new HashMap<>();
+    	Map<String, Integer> countyCase = new HashMap<>();
     	
         for( Covid19Case covidCase: covid19case) {
-    	    String county = covidCase.getCounty();
             int currentDateCases = covidCase.getNumberOfCases();
-          
-    		if(caseCount.keySet().contains(county)) { 
-    			currentDateCases += caseCount.get(county);
-    		}
-    		caseCount.put(county, currentDateCases);
+            String county = covidCase.getCounty();
+            int accCaseCount = accCaseCount(currentDateCases, countyCase, county);
+            
+    		countyCase.put(county, accCaseCount);
     	}
-        return caseCount;
+        return countyCase;
     }
     
     
@@ -49,40 +34,38 @@ public class Answer {
     	Map<Integer, Map<String, Integer> > caseOfMonth = new HashMap<>();
     	
     	for(Covid19Case covidCase: covid19case) {
-    		System.out.println()
     		int month = covidCase.getDate().getMonthValue();
     		String county = covidCase.getCounty();
     		int currentDateCases = covidCase.getNumberOfCases();
     		
-    		Map<String, Integer> caseCount = new HashMap<>();
-    		if(caseOfMonth.keySet().contains(month)) {
-    			if(caseCount.keySet().contains(county)) { 
-    				currentDateCases += caseCount.get(county);
-    			}
-    			caseCount.put(county, currentDateCases);
-    		}else {
-    			if(caseCount.keySet().contains(county)) { 
-    				currentDateCases += caseCount.get(county);
-    			}
-    			caseCount.put(county, currentDateCases);
-    			caseOfMonth.put(month, caseCount);
+    		Map<String, Integer> countyCase = caseOfMonth.get(month);
+    		
+    		if(Objects.isNull(countyCase)) {
+    			countyCase = new HashMap<>();
+    			countyCase.put(county, 0);
+    			caseOfMonth.put(month, countyCase);
     		}
-    	
-    	
-    	
-    	
+    		
+    		int accCaseCount = accCaseCount(currentDateCases, countyCase, county);
+    		
+            countyCase.put(county, accCaseCount);
+    	    caseOfMonth.put(month, countyCase);	 
     	}
- 
-    	
-    	
-    	
-    	
+
     	return caseOfMonth;
     }
     
+    private static int accCaseCount(int currentDateCases, Map<String, Integer> countyCase, String county) {
+		int accCaseCount = currentDateCases;
+		if(countyCase.keySet().contains(county)) { 
+			accCaseCount += countyCase.get(county);
+		}
+		return accCaseCount;
+    }
+     
     private static int answerThree(List<Covid19Case> covid19case) {
     	//3. 取台北在六月份的新冠病毒案例
-    	int totalCases = 0;
+    	int accCaseCount = 0;
     	
     	for(Covid19Case covidCase : covid19case) {
     		String county = covidCase.getCounty();
@@ -90,12 +73,10 @@ public class Answer {
     		int caseCount = covidCase.getNumberOfCases();
     		
     		if(county.equals("Taipei") && month == 6) {
-    			totalCases += caseCount;
+    			accCaseCount += caseCount;
     		}
     	}
-    	
-    	return totalCases;
+    	return accCaseCount;
     }
-    
     
 }
